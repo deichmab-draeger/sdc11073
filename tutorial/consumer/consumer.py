51ea6bd31dec881991e47a251bc1a866708ee7c1
Deichmann <Bernd.Deichmann@draeger.com>
2023-05-23 11:01:55 +0200

    fixed tutorial code consumer and provider

--- tutorial/consumer/consumer.py
+++ tutorial/consumer/consumer.py
@@ -1,12 +1,17 @@
 import time
+import traceback
 import uuid
-from sdc11073.xml_types import pm_types
-from sdc11073.xml_types import pm_qnames as pm
-from sdc11073.wsdiscovery import WSDiscoverySingleAdapter
-from sdc11073.definitions_sdc import SDC_v1_Definitions
-from sdc11073.sdcclient import SdcClient
-from sdc11073.mdib import ClientMdibContainer
+
 from sdc11073 import observableproperties
+from sdc11073.definitions_sdc import SDC_v1_Definitions
+from sdc11073.mdib import ClientMdibContainer
+from sdc11073.sdcclient import SdcClient
+from sdc11073.wsdiscovery import WSDiscoverySingleAdapter
+from sdc11073.xml_types import pm_qnames as pm
+from sdc11073.xml_types import pm_types
+from sdc11073.loghelper import basic_logging_setup
+
+
 # This example shows how to implement a very simple SDC Consumer (client)
 # It will scan for SDC Providers and connect to on well known UUID
 
@@ -15,81 +20,86 @@ from sdc11073 import observableproperties
 baseUUID = uuid.UUID('{cc013678-79f6-403c-998f-3cc0cc050230}')
 device_A_UUID = uuid.uuid5(baseUUID, "12345")
 
-# callback function that will be called upon metric updates from the provider
-def onMetricUpdate(metricsByHandle):
-    # we get all changed handles as parameter, iterate over them and output
-    for oneHandle in metricsByHandle:
-        print("Got update on: {}".format(oneHandle))
 
-def setEnsembleContext(theMDIB, theClient):
-    # calling operation on remote device 
+# callback function that will be called upon metric updates from the provider
+def on_metric_update(metrics_by_handle):
+    # we get all changed handles as parameter, iterate over them and output
+    for one_handle in metrics_by_handle:
+        print("Got update on: {}".format(one_handle))
+
+
+def set_ensemble_context(the_mdib: ClientMdibContainer, the_client: SdcClient):
+    # calling operation on remote device
     print("Trying to set ensemble context of device A")
     # first we get the container to the element in the MDIB
-    descriptorContainer = theMDIB.descriptions.NODETYPE.getOne(pm.EnsembleContextDescriptor)
+    descriptor_container = the_mdib.descriptions.NODETYPE.get_one(pm.EnsembleContextDescriptor)
     # get the context of our provider(client)
-    contextClient = theClient.ContextService_client
+    context_client = the_client.context_service_client
     # start with empty operation handle and try to find the one we need
-    operationHandle = None
+    operation_handle = None
     # iterate over all matching handles (can be 0..n)
-    for oneOp in theMDIB.descriptions.NODETYPE.get(pm.SetContextStateOperationDescriptor, []):
-        if oneOp.OperationTarget == descriptorContainer.handle:
-            operationHandle = oneOp.Handle
-    # now we should have a operatoin handle to work with
+    for one_op in the_mdib.descriptions.NODETYPE.get(pm.SetContextStateOperationDescriptor, []):
+        if one_op.OperationTarget == descriptor_container.Handle:
+            operation_handle = one_op.Handle
+    # now we should have an operation handle to work with
     # create a new ensemble context as parameter to this operation
-    newEnsembleContext = contextClient.mkProposedContextObject(descriptorContainer.handle)
-    newEnsembleContext.ContextAssociation = 'Assoc'
-    newEnsembleContext.Identification = [
+    new_ensemble_context = context_client.mk_proposed_context_object(descriptor_container.Handle)
+    new_ensemble_context.ContextAssociation = pm_types.ContextAssociation.ASSOCIATED
+    new_ensemble_context.Identification = [
         pm_types.InstanceIdentifier(root="1.2.3", extension_string="SupervisorSuperEnsemble")]
     # execute the remote operation (based on handle) with the newly created ensemble as parameter
-    contextClient.setContextState(operationHandle, [newEnsembleContext])
+    context_client.set_context_state(operation_handle, [new_ensemble_context])
 
 
 # main entry, will start to scan for the known provider and connect
-# runs forever and consumes metrics everafter
+# runs forever and consumes metrics ever after
 if __name__ == '__main__':
-    # start with discovery (MDPWS) that is running on the named adapter "Ethernet" (replace as you need it on your machine, e.g. "enet0" or "Ethernet)
-    myDiscovery = WSDiscoverySingleAdapter("Ethernet")
+    basic_logging_setup()  # enable logging
+
+    # start with discovery (MDPWS) that is running on the named adapter "Ethernet"
+    # (replace as you need it on your machine, e.g. "enet0" or "Ethernet)
+    my_discovery = WSDiscoverySingleAdapter("Ethernet")
     # start the discovery
-    myDiscovery.start()
+    my_discovery.start()
     # we want to search until we found one device with this client
-    foundDevice = False
+    found_device = False
     # loop until we found our provider
-    while not foundDevice:
+    while not found_device:
         # we now search explicitly for MedicalDevices on the network
         # this will send a probe to the network and wait for responses
         # See MDPWS discovery mechanisms for details
-        services = myDiscovery.search_services(types=SDC_v1_Definitions.MedicalDeviceTypesFilter)
+        services = my_discovery.search_services(types=SDC_v1_Definitions.MedicalDeviceTypesFilter)
 
         # now iterate through the discovered services to check if we foundDevice
         # the specific provider we search for
-        for oneService in services:
+        for one_service in services:
             try:
-                print("Got service: {}".format(oneService.getEPR()))
+                print("Got service: {}".format(one_service.epr))
                 # the EndPointReference is created based on the UUID of the Provider
-                if oneService.getEPR() == device_A_UUID.urn:
-                    print("Got a match: {}".format(oneService))
+                if one_service.epr == device_A_UUID.urn:
+                    print("Got a match: {}".format(one_service))
                     # now create a new SDCClient (=Consumer) that can be used
                     # for all interactions with the communication partner
-                    my_client = SdcClient.from_wsd_service(oneService)
+                    my_client = SdcClient.from_wsd_service(one_service, ssl_context=None)
                     # start all services on the client to make sure we get updates
-                    my_client.startAll()
+                    my_client.start_all()
                     # all data interactions happen through the MDIB (MedicalDeviceInformationBase)
                     # that contains data as described in the BICEPS standard
                     # this variable will contain the data from the provider
                     my_mdib = ClientMdibContainer(my_client)
                     my_mdib.init_mdib()
-                    # we can subscribe to updates in the MDIB through the 
+                    # we can subscribe to updates in the MDIB through the
                     # Observable Properties in order to get a callback on
                     # specific changes in the MDIB
-                    observableproperties.bind(my_mdib, metricsByHandle=onMetricUpdate)
-                    # in order to end the 'scan' loop 
-                    foundDevice = True
+                    observableproperties.bind(my_mdib, metrics_by_handle=on_metric_update)
+                    # in order to end the 'scan' loop
+                    found_device = True
 
                     # now we demonstrate how to call a remote operation on the consumer
-                    setEnsembleContext(my_mdib, my_client)
+                    set_ensemble_context(my_mdib, my_client)
             except:
+                print(traceback.format_exc())
                 print("Problem in discovery, ignoring it")
-
 
     # endless loop to keep the client running and get notified on metric changes through callback
     while True:
--- tutorial/provider/provider.py
+++ tutorial/provider/provider.py
@@ -1,13 +1,18 @@
-import uuid
 import time
-from sdc11073.xml_types import pm_types
-from sdc11073.xml_types import pm_qnames as pm
-from sdc11073.sdcdevice import SdcDevice
-from sdc11073.mdib import DeviceMdibContainer
-from sdc11073.xml_types.dpws_types import ThisDeviceType, ThisModelType
+import uuid
+from decimal import Decimal
+
 from sdc11073.location import SdcLocation
+from sdc11073.mdib import DeviceMdibContainer
+from sdc11073.mdib.statecontainers import EnsembleContextStateContainer
+from sdc11073.sdcdevice import SdcDevice
 from sdc11073.wsdiscovery import WSDiscoverySingleAdapter
-# example SDC provider (device) that sends out metrics every now and then
+from sdc11073.xml_types import pm_qnames as pm
+from sdc11073.xml_types import pm_types
+from sdc11073.xml_types.dpws_types import ThisDeviceType, ThisModelType
+from sdc11073.loghelper import basic_logging_setup
+
+# example SDC provider (device) that sends out metrics frequently
 
 
 # The provider we use, should match the one in consumer example
@@ -16,82 +21,84 @@ baseUUID = uuid.UUID('{cc013678-79f6-403c-998f-3cc0cc050230}')
 my_uuid = uuid.uuid5(baseUUID, "12345")
 
 
-
 # setting the local ensemble context upfront
-def setLocalEnsembleContext(mdib, ensemble):
-    descriptorContainer = mdib.descriptions.NODETYPE.getOne(pm.EnsembleContextDescriptor)
-    if not descriptorContainer:
+def set_local_ensemble_context(mdib: DeviceMdibContainer, ensemble: str):
+    descriptor_container = mdib.descriptions.NODETYPE.getOne(pm.EnsembleContextDescriptor)
+    if not descriptor_container:
         print("No ensemble contexts in mdib")
         return
-    allEnsembleContexts = mdib.contextStates.descriptorHandle.get(descriptorContainer.handle, [])
-    with mdib.mdibUpdateTransaction() as mgr:
+    all_ensemble_contexts = mdib.context_states.descriptorHandle.get(descriptor_container.Handle, [])
+    with mdib.transaction_manager() as my_mgr:
         # set all to currently associated Locations to Disassociated
-        associatedEnsembles = [l for l in allEnsembleContexts if
-                               l.ContextAssociation == pm_types.ContextAssociation.ASSOCIATED]
-        for l in associatedEnsembles:
-            ensembleContext = mgr.getContextState(l.descriptorHandle, l.Handle)
-            ensembleContext.ContextAssociation = pm_types.ContextAssociation.DISASSOCIATED
-            ensembleContext.UnbindingMdibVersion = mdib.mdibVersion  # UnbindingMdibVersion is the first version in which it is no longer bound ( == this version)
+        associated_ensembles = [l for l in all_ensemble_contexts if
+                                l.ContextAssociation == pm_types.ContextAssociation.ASSOCIATED]
+        for l in associated_ensembles:
+            ensemble_context = my_mgr.get_context_state(l.descriptorHandle, l.Handle)
+            ensemble_context.ContextAssociation = pm_types.ContextAssociation.DISASSOCIATED
+            # UnbindingMdibVersion is the first version in which it is no longer bound ( == this version)
+            ensemble_context.UnbindingMdibVersion = mdib.mdib_version
 
-        newEnsState = mgr.get_state(descriptorContainer.handle)  # this creates a new location state
-        newEnsState.ContextAssociation = 'Assoc'
-        newEnsState.Identification = [pm_types.InstanceIdentifier(root="1.2.3", extension_string=ensemble)]
+        new_ens_state = EnsembleContextStateContainer(descriptor_container)
+        new_ens_state.Handle = uuid.uuid4().hex
+        new_ens_state.ContextAssociation = pm_types.ContextAssociation.ASSOCIATED
+        new_ens_state.Identification = [pm_types.InstanceIdentifier(root="1.2.3", extension_string=ensemble)]
+        my_mgr.add_state(new_ens_state)
 
 
 if __name__ == '__main__':
-    # start with discovery (MDPWS) that is running on the named adapter "Ethernet" (replace as you need it on your machine, e.g. "enet0" or "Ethernet")
-    myDiscovery = WSDiscoverySingleAdapter("Ethernet")
+    basic_logging_setup()  # enable logging
+    # start with discovery (MDPWS) that is running on the named adapter "Ethernet"
+    # (replace as you need it on your machine, e.g. "enet0" or "Ethernet")
+    my_discovery = WSDiscoverySingleAdapter("Ethernet")
     # start the discovery
-    myDiscovery.start()
-    # create a local mdib that will be sent out on the network, the mdib is based on a XML file
+    my_discovery.start()
+    # create a local mdib that will be sent out on the network, the mdib is based on an XML file
     my_mdib = DeviceMdibContainer.from_mdib_file("mdib.xml")
-    print ("My UUID is {}".format(my_uuid))
+    print("My UUID is {}".format(my_uuid))
     # set a location context to allow easy discovery
     my_location = SdcLocation(fac='HOSP', poc='CU2', bed='BedSim')
     # set model information for discovery
-    dpwsModel = ThisModelType(manufacturer='Draeger',
-                              manufacturer_url='www.draeger.com',
-                              model_name='TestDevice',
-                              model_number='1.0',
-                              model_url='www.draeger.com/model',
-                              presentation_url='www.draeger.com/model/presentation')
-    dpwsDevice = ThisDeviceType(friendly_name='TestDevice',
-                                firmware_version='Version1',
-                                serial_number='12345')
+    this_model = ThisModelType(manufacturer='Draeger',
+                               manufacturer_url='www.draeger.com',
+                               model_name='TestDevice',
+                               model_number='1.0',
+                               model_url='www.draeger.com/model',
+                               presentation_url='www.draeger.com/model/presentation')
+    this_device = ThisDeviceType(friendly_name='TestDevice',
+                                 firmware_version='Version1',
+                                 serial_number='12345')
     # create a device (provider) class that will do all the SDC magic
-    sdcDevice = SdcDevice(ws_discovery=myDiscovery,
-                          epr=my_uuid,
-                          this_model=dpwsModel,
-                          this_device=dpwsDevice,
-                          device_mdib_container=my_mdib)
+    sdc_device = SdcDevice(ws_discovery=my_discovery,
+                           epr=my_uuid,
+                           this_model=this_model,
+                           this_device=this_device,
+                           device_mdib_container=my_mdib)
     # start the local device and make it discoverable
-    sdcDevice.start_all()
+    sdc_device.start_all()
     # set the local ensemble context to ease discovery based on ensemble ID
-    setLocalEnsembleContext(my_mdib, "MyEnsemble")
+    set_local_ensemble_context(my_mdib, "MyEnsemble")
     # set the location on our device
-    sdcDevice.set_location(my_location)
-    # create one local numeric metric that will change later on
-    numMetrDescr = pm.NumericMetricDescriptor
+    sdc_device.set_location(my_location)
     # get all metrics from the mdib (as described in the file)
-    allMetricDescrs = [c for c in my_mdib.descriptions.objects if c.NODETYPE == numMetrDescr]
+    all_metric_descriptors = my_mdib.descriptions.NODETYPE.get(pm.NumericMetricDescriptor)
     # now change all the metrics in one transaction
-    with my_mdib.mdibUpdateTransaction() as mgr:
-        for metricDescr in allMetricDescrs:
+    with my_mdib.transaction_manager() as mgr:
+        for metric_descr in all_metric_descriptors:
             # get the metric state of this specific metric
-            st = mgr.get_state(metricDescr.handle)
+            st = mgr.get_state(metric_descr.Handle)
             # create a value in case it is not there yet
-            st.mkMetricValue()
+            st.mk_metric_value()
             # set the value and some other fields to a fixed value
-            st.metricValue.Value = 1.0
-            st.metricValue.ActiveDeterminationPeriod = "1494554822450"
-            st.metricValue.Validity = 'Vld'
-            st.ActivationState = "On"
-    metricValue = 0
+            st.MetricValue.Value = Decimal(1.0)
+            st.MetricValue.ActiveDeterminationPeriod = 1494554822450
+            st.MetricValue.Validity = pm_types.MeasurementValidity.VALID
+            st.ActivationState = pm_types.ComponentActivation.ON
+    metric_value = Decimal(0)
     # now iterate forever and change the value every few seconds
     while True:
-        metricValue += 1
-        with my_mdib.mdibUpdateTransaction() as mgr:
-            for metricDescr in allMetricDescrs:
-                st = mgr.get_state(metricDescr.handle)
-                st.metricValue.Value = metricValue
+        metric_value += 1
+        with my_mdib.transaction_manager() as mgr:
+            for metric_descr in all_metric_descriptors:
+                st = mgr.get_state(metric_descr.Handle)
+                st.MetricValue.Value = metric_value
         time.sleep(5)
